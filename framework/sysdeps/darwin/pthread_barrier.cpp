01b001d4bde461336d4f272c600ba45716340687
Thiago Macieira <thiago.macieira@intel.com>
2021-10-20 16:07:35 -0700

    FW/Mac: fix build: Apple hasn't added POSIX.1-2008 barrier support

---
+++ framework/sysdeps/darwin/pthread_barrier.cpp
@@ -0,0 +1,34 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "pthread_barrier.h"
+
+int pthread_barrier_destroy(pthread_barrier_t *barrier)
+{
+    pthread_mutex_destroy(&barrier->mutex);
+    pthread_cond_destroy(&barrier->cond);
+    return 0;
+}
+
+int pthread_barrier_init(pthread_barrier_t *barrier,
+                         const pthread_barrierattr_t *attr, unsigned count)
+{
+    pthread_mutex_init(&barrier->mutex, nullptr);
+    pthread_cond_init(&barrier->cond, nullptr);
+    barrier->count = count;
+    return 0;
+}
+
+int pthread_barrier_wait(pthread_barrier_t *barrier)
+{
+    pthread_mutex_lock(&barrier->mutex);
+    if (--barrier->count)
+        pthread_cond_wait(&barrier->cond, &barrier->mutex);
+    else
+        pthread_cond_broadcast(&barrier->cond);
+    pthread_mutex_unlock(&barrier->mutex);
+
+    // Note: we don't return PTHREAD_BARRIER_SERIAL_THREAD
+    return 0;
+}
---
+++ framework/sysdeps/darwin/pthread_barrier.h
@@ -0,0 +1,30 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef DARWIN_PTHREAD_BARRIER_H
+#define DARWIN_PTHREAD_BARRIER_H
+
+#include <pthread.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct pthread_barrier {
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+    unsigned count;
+} pthread_barrier_t;
+typedef struct pthread_barrierattr pthread_barrierattr_t;
+
+int pthread_barrier_destroy(pthread_barrier_t *barrier);
+int pthread_barrier_init(pthread_barrier_t *barrier,
+                         const pthread_barrierattr_t *attr, unsigned count);
+int pthread_barrier_wait(pthread_barrier_t *barrier);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // DARWIN_PTHREAD_BARRIER_H
