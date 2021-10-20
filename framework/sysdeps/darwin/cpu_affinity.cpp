4d75ef9d31f0e95abe2241ddde4d94146e81b801
Thiago Macieira <thiago.macieira@intel.com>
2021-10-20 15:59:29 -0700

    Meson/Mac: add support for "darwin" target

---
+++ framework/sysdeps/darwin/cpu_affinity.cpp
@@ -0,0 +1,33 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "topology.h"
+
+#include <stdio.h>
+#include <unistd.h>
+
+LogicalProcessorSet ambient_logical_processor_set()
+{
+    static bool warning_printed = []() {
+        fprintf(stderr, "# WARNING: this OS does not support thread affinity. Results may be unreliable.\n");
+        return true;
+    }();
+    (void) warning_printed;
+
+    // assume all CPUs
+    LogicalProcessorSet result = {};
+    long n = sysconf(_SC_NPROCESSORS_ONLN);
+    for (long i = 0; i < n; ++i)
+        result.set(LogicalProcessor(i));
+    return result;
+}
+
+bool pin_to_logical_processor(LogicalProcessor n, const char *thread_name)
+{
+    // simulate success
+    (void) n;
+    (void) thread_name;
+    return true;
+}
+
