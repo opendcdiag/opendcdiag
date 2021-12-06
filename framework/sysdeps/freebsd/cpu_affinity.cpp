opendcdiag/opendcdiag

commit 94f823d5ffd026ba7173117763c99e56b7fe362b

FW/FreeBSD: fix build: update cpu_affinity.cpp to match the post-Windows state

framework/sysdeps/freebsd/cpu_affinity.cpp
@@ -1,8 +1,14 @@
 /*
- * SPDX-License-Identifier: Apache-2.0
+ * (C) 2019-2021 Intel Corporation
+ *
+ * This source code is Intel Confidential and not for distribution outside of Intel without
+ * explicit permission from the authors
+ *
+ * sandstone test framework and tests
+ *
  */
 
-#include <cpu_affinity.h>
+#include <topology.h>
 
 #include <pthread_np.h>
 #include <sys/cpuset.h>
@@ -12,8 +18,8 @@ static_assert(sizeof(cpuset_t) >= sizeof(LogicalProcessorSet));
 LogicalProcessorSet ambient_logical_processor_set()
 {
     LogicalProcessorSet result;
-    if (cpuset_getaffinity(CPU_LEVEL_ROOT, CPU_WHICH_PID, -1, sizeof(result.array),
-                           reinterpret_cast<cpu_set_t *>(result.array)) != 0)
+    if (cpuset_getaffinity(CPU_LEVEL_WHICH, CPU_WHICH_PID, -1, sizeof(result.array),
+                           reinterpret_cast<cpuset_t *>(result.array)) != 0)
         result.clear();
     return result;
 }
@@ -25,9 +31,9 @@ bool pin_to_logical_processor(LogicalProcessor n, const char *thread_name)
 
     cpuset_t cpu_set;
     CPU_ZERO(&cpu_set);
-    CPU_SET(n, &cpu_set);
+    CPU_SET(int(n), &cpu_set);
 
-    if (cpuset_setaffinity(CPU_LEVEL_ROOT, CPU_WHICH_PID, -1, sizeof(cpu_set), &cpu_set)) {
+    if (cpuset_setaffinity(CPU_LEVEL_WHICH, CPU_WHICH_TID, -1, sizeof(cpu_set), &cpu_set)) {
         perror("cpuset_setaffinity");
         return false;
     }

framework/topology.h
@@ -58,7 +58,11 @@ enum class LogicalProcessor : int {};
 class LogicalProcessorSet
 {
 public:
+#if defined(__linux__) || defined(_WIN32)
     static constexpr int Size = 1024;
+#else
+    static constexpr int Size = 256;
+#endif
 
     using Word = unsigned long long;
     Word array[Size / (CHAR_BIT * sizeof(Word))];

commit 559e91afd2787ac5efca46ca3dbdd3a3abd43f3a

FW: sysdeps: use correct license header
Fixes: 94f823d5ffd0 ("FW/FreeBSD: fix build: update cpu_affinity.cpp to match the post-Windows state")
Signed-off-by: Joe Konno <joe.konno@intel.com>

framework/sysdeps/freebsd/cpu_affinity.cpp
@@ -1,11 +1,5 @@
 /*
- * (C) 2019-2021 Intel Corporation
- *
- * This source code is Intel Confidential and not for distribution outside of Intel without
- * explicit permission from the authors
- *
- * sandstone test framework and tests
- *
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <topology.h>