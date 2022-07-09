b9e491abfa5cdea5089c5d7428a609472d6ac95a
Thiago Macieira <thiago.macieira@intel.com>
2022-07-08 17:19:11 -0700

    FW: don't wait too long between test starts
    We don't need to wait for the whole period, only so much that the
    condition will be true when we wake up.
    
    Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

--- framework/sandstone.cpp
+++ framework/sandstone.cpp
@@ -2794,19 +2794,20 @@ static void background_scan_wait()
 
     // Don't run too many tests in a short period of time
     Duration elapsed = now - sApp->background_scan.timestamp.back();
-    if (elapsed < DelayBetweenTestBatch) {
+    if (Duration expected_start = DelayBetweenTestBatch - elapsed; expected_start > 0s) {
+        expected_start += MinimumDelayBetweenTests;
         logging_printf(LOG_LEVEL_VERBOSE(2), "# Background scan: %zu tests completed in "
-                                             "%d s, waiting %d +/- 0.1%% s\n",
-                       sApp->background_scan.timestamp.size(),
-                       as_seconds(elapsed), as_seconds(DelayBetweenTestBatch));
-        do_wait(DelayBetweenTestBatch, DelayBetweenTestBatch / 1000);
+                                             "%d s, waiting %d +/- %d s\n",
+                       sApp->background_scan.timestamp.size(), as_seconds(elapsed),
+                       as_seconds(expected_start), as_seconds(MinimumDelayBetweenTests));
+        do_wait(expected_start, MinimumDelayBetweenTests);
         goto skip_wait;
     }
 
     logging_printf(LOG_LEVEL_VERBOSE(3), "# Background scan: waiting %d +/- 10%% s\n",
                    as_seconds(MinimumDelayBetweenTests));
     while (1) {
-        do_wait(MinimumDelayBetweenTests, MinimumDelayBetweenTests / 15);
+        do_wait(MinimumDelayBetweenTests, MinimumDelayBetweenTests / 10);
 
 skip_wait:
         now = MonotonicTimePoint::clock::now();
