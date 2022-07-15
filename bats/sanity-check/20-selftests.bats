opendcdiag/opendcdiag

commit 02c21ef5155d8f9d8b4891fb76555b262aae52d2

FW: implement a built-in test list
This replaces the previous weighted test list, which was missing, with a
simple sequential order.

Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

framework/meson.build
@@ -38,6 +38,24 @@ generated_cpu_features_h = configure_file(
 	],
 )
 
+generated_builtin_test_list = []
+builtin_test_list_inputs = []
+foreach f : get_option('builtin_test_list')
+	builtin_test_list_inputs += meson.project_source_root() / f
+endforeach
+if builtin_test_list_inputs.length() != 0
+	generated_builtin_test_list = custom_target(
+		'sandstone_ordered_test_list.h',
+		input : builtin_test_list_inputs,
+		output : [
+			'sandstone_ordered_test_list.h'
+		],
+		command : [
+			perl, files('scripts/generate-test-list.pl'), '@OUTPUT@', '@INPUT@',
+		],
+	)
+endif
+
 if cc.has_type('_Float16') and cpp.has_type('_Float16')
         framework_config.set('SANDSTONE_FP16_TYPE', '_Float16')
 elif cc.has_type('__fp16') and cpp.has_type('__fp16')
@@ -47,6 +65,7 @@ endif
 framework_config.set10('SANDSTONE_GA', get_option('flavor') == 'ga')
 framework_config.set10('SANDSTONE_GA_DEV', get_option('flavor') == 'ga-dev')
 
+framework_config.set10('SANDSTONE_BUILTIN_TEST_LIST', get_option('builtin_test_list').length() != 0)
 framework_config.set_quoted('SANDSTONE_EXECUTABLE_NAME', get_option('executable_name'))
 framework_config.set_quoted('SANDSTONE_FALLBACK_EXEC', get_option('fallback_exec'))
 
@@ -119,6 +138,8 @@ endif
 framework_a = static_library(
 	'framework',
 	framework_files,
+	generated_builtin_test_list,
+	generated_cpu_features_h,
 	build_by_default: false,
 	include_directories : [
 		framework_incdir,

framework/sandstone.cpp
@@ -70,6 +70,12 @@
 #include "sandstone_utils.h"
 #include "topology.h"
 
+#if SANDSTONE_BUILTIN_TEST_LIST
+#  include "sandstone_ordered_test_list.h"
+#else
+static constexpr struct test *ordered_test_list[] = { nullptr };
+#endif
+
 #ifdef _WIN32
 #  include <ntstatus.h>
 #  include <shlwapi.h>
@@ -163,6 +169,7 @@ enum {
     timeout_option,
     total_retest_on_failure,
     ud_on_failure_option,
+    use_builtin_test_list_option,
     use_predictable_file_names_option,
     version_option,
     weighted_testrun_option,
@@ -2907,6 +2914,7 @@ int main(int argc, char **argv)
         { "total-retest-on-failure", required_argument, nullptr, total_retest_on_failure },
         { "total-time", required_argument, nullptr, 'T' },
         { "ud-on-failure", no_argument, nullptr, ud_on_failure_option },
+        { "use-builtin-test-list", no_argument, nullptr, use_builtin_test_list_option },
         { "verbose", no_argument, nullptr, 'v' },
         { "version", no_argument, nullptr, version_option },
         { "weighted-testrun-type", required_argument, nullptr, weighted_testrun_option },
@@ -2938,6 +2946,7 @@ int main(int argc, char **argv)
     // test selection
     const char *test_list_file_path = nullptr;
     bool test_list_randomize = false;
+    bool use_builtin_test_list = false;
     int starting_test_number = 1;  // One based count for user interface, not zero based
     int ending_test_number = INT_MAX;
     WeightedTestScheme test_selection_strategy = Alphabetical;
@@ -3159,6 +3168,15 @@ int main(int argc, char **argv)
         case ud_on_failure_option:
             sApp->ud_on_failure = true;
             break;
+        case use_builtin_test_list_option:
+            if (SandstoneConfig::HasBuiltinTestList) {
+                use_builtin_test_list = true;
+            } else {
+                fprintf(stderr, "%s: --use-builtin-test-list specified but this build does not "
+                                "have a built-in test list.", argv[0]);
+                return EX_USAGE;
+            }
+            break;
         case use_predictable_file_names_option:
 #ifndef NDEBUG
             sApp->use_predictable_file_names = true;
@@ -3366,6 +3384,9 @@ int main(int argc, char **argv)
         sApp->thermal_throttle_temp = INT_MIN;
         sApp->verbosity = -1;
         fatal_errors = true;
+        use_builtin_test_list = true;
+        static_assert(!SandstoneConfig::RestrictedCommandLine || SandstoneConfig::HasBuiltinTestList,
+                "Restricted command-line build must have a built-in test list");
     }
 
     if (optind < argc) {
@@ -3407,6 +3428,10 @@ int main(int argc, char **argv)
 
     // If we want to use the weighted testrunner we need to initialize it
     if (test_list_file_path) {
+        if (use_builtin_test_list)
+            logging_printf(LOG_LEVEL_QUIET,
+                           "# WARNING: both --test-list-file and --use-builtin-test-list "
+                           "specified, using test file \"%s\".\n", test_list_file_path);
         if (test_list.size()) {
             logging_printf(LOG_LEVEL_QUIET,
                            "# WARNING: both --test-list-file and --enable specified, using only "
@@ -3418,10 +3443,20 @@ int main(int argc, char **argv)
                                                        starting_test_number, ending_test_number,
                                                        test_list_randomize);
     } else {
-        generate_test_list(test_list);
         if (test_list_randomize) {
             logging_printf(LOG_LEVEL_QUIET, "# WARNING: --test-list-randomize used without "
                                             "--test-list-file. Ignored.\n");
+        }
+
+        if (use_builtin_test_list) {
+            if (!SandstoneConfig::RestrictedCommandLine && test_list.size()) {
+                logging_printf(LOG_LEVEL_QUIET,
+                               "# WARNING: both --enable and --use-builtin-test-list specified, "
+                               "the built-in test list.\n");
+            }
+            test_list = { std::begin(ordered_test_list), std::end(ordered_test_list) };
+        } else {
+            generate_test_list(test_list);
         }
 
         if (!test_selector)

framework/sandstone_config.h.in
@@ -20,6 +20,7 @@
 #mesondefine SANDSTONE_EXECUTABLE_NAME
 #mesondefine SANDSTONE_FALLBACK_EXEC
 #mesondefine SANDSTONE_STATIC
+#mesondefine SANDSTONE_BUILTIN_TEST_LIST
 
 #mesondefine SANDSTONE_GA
 #mesondefine SANDSTONE_GA_DEV
@@ -43,6 +44,7 @@ static constexpr bool GeneralAvailability = SANDSTONE_GA;
 static constexpr bool ChildBacktrace = SANDSTONE_CHILD_BACKTRACE;
 static constexpr bool NoLogging = SANDSTONE_NO_LOGGING;
 static constexpr bool RestrictedCommandLine = SANDSTONE_RESTRICTED_CMDLINE;
+static constexpr bool HasBuiltinTestList = SANDSTONE_BUILTIN_TEST_LIST;
 } // namespace SandstoneConfig
 
 #endif /* __cplusplus */

framework/scripts/generate-test-list.pl
@@ -1,0 +1,51 @@
+#!/usr/bin/perl
+# Copyright 2022 Intel Corporation.
+# SPDX-License-Identifier: Apache-2.0
+
+use strict;
+my @test_order;
+my $output = shift @ARGV;
+unless ($output eq "-") {
+    open OUTPUT, ">", $output;
+    select OUTPUT;
+}
+print '/* This is a generated file. DO NOT EDIT! */
+
+#ifndef TEST_LIST_H
+#define TEST_LIST_H
+
+#include "sandstone_tests.h"
+
+';
+
+# Collect the test list
+push @ARGV, "/dev/stdin" unless scalar @ARGV;
+while ($_ = shift @ARGV) {
+    open LIST, "<", $_ or die("Could not open $_: $!");
+    while (<LIST>) {
+        s/^\s+//;               # remove leading space
+        s/#.*$//;               # remove comments
+        s/\s+$//;               # remove trailing space
+        next if /^$/;           # skip empty lines
+        push @test_order, $_;
+    }
+    close LIST;
+}
+
+# Make sorted a list of all unique tests
+my %alltests = map { ($_ => 1) } @test_order;
+
+# Print all test declarations
+for my $test (sort keys %alltests) {
+    printf "extern struct test _test_%s;\n", $test;
+}
+
+# Print the test order
+print "\nstatic constexpr struct test *ordered_test_list[] = {\n";
+for my $test (@test_order) {
+    printf "    \&_test_%s,\n", $test;
+}
+
+print '};
+#endif /* TEST_LIST_H */
+';

commit 9f0a3d528f234dc2aa126bda9b81f1599502d753

bats: add a test for --test-file-list
To prevent a silly regression like what happened in PR #116 (commit
@d0d1227e609c7e4a779a3d64775aa67d6f0605a2).

Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

bats/sanity-check/20-selftests.bats
@@ -345,6 +345,62 @@ test_yaml_regexp() {
     test_yaml_regexp "/tests/0/threads/0/messages/2/text" '.*Numbers: 1 4097'
 }
 
+test_list_file() {
+    local -a list=("$@")
+    local -i count=${#list[@]}
+    declare -A yamldump
+    sandstone_selftest --test-list-file <(printf '%s\n' "${list[@]}")
+    [[ "$status" -eq 0 ]]
+    test_yaml_regexp "/exit" pass
+
+    # confirm each test
+    local -i i=0
+    local entry
+    for entry in "${list[@]}"; do
+        if [[ "$entry" = "" ]] || [[ "$entry" = "#"* ]]; then
+            continue;
+        fi
+
+        test_yaml_regexp "/tests/$i/result" "pass"
+        test_yaml_regexp "/tests/$i/test" "${entry%:*}"
+
+        # was there a duration?
+        local -i duration=${entry#*:}
+        if (( duration > 0 )); then
+            test_yaml_numeric "/tests/$i/test-runtime" "value >= $duration"
+            test_yaml_numeric "/tests/$i/test-runtime" "value <= 2 * $duration"
+        fi
+
+        i=$((i + 1))
+    done
+    test_yaml_numeric "/tests@len" "value = $i"
+}
+
+@test "--test-list-file with 1 test" {
+    test_list_file selftest_pass
+}
+
+@test "--test-list-file with 3 tests" {
+    test_list_file selftest_pass selftest_logs selftest_pass
+}
+
+@test "--test-list-file with duration" {
+    test_list_file selftest_pass:default selftest_timedpass:50
+}
+
+@test "--test-list-file with comments and empty lines" {
+    test_list_file '# a file list' '' selftest_pass '' '# the end!'
+}
+
+@test "--test-list-file with unknown test name" {
+    # This doesn't produce valid YAML output, so run directly
+    local name=`mktemp -u selftest_XXXXXX`
+    run $SANDSTONE -Y -o - --selftests --test-list-file <(echo "$name")
+    echo "$output"
+    [[ $status -eq 64 ]]        # exit(EX_USAGE)
+    grep -qwF "$name" <<<"$output"
+}
+
 # -- negative tests --
 
 @test "selftest_failinit" {