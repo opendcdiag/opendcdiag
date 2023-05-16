a6a99bfe713ca823b0f6cf67867cfba6b86969cf
Arzhan Kinzhalin <arzhan.i.kinzhalin@intel.com>
2023-05-15 21:07:50 -0700

    Clean up: generate_test_list.py: use single quotes.
    Signed-off-by: Arzhan Kinzhalin <arzhan.i.kinzhalin@intel.com>

--- framework/scripts/generate_test_list.py
+++ framework/scripts/generate_test_list.py
@@ -36,12 +36,12 @@ def main():
     if (config_str == ''):
         config = {}
     else:
-        config = config_str.split(",")
+        config = config_str.split(',')
     default_name = None
     for pair in config:
-        parsed = pair.split(":")
+        parsed = pair.split(':')
         if len(parsed) != 2:
-            print(f"Incorrect configuration string: {pair}")
+            print(f'Incorrect configuration string: {pair}')
             exit(2)
         name = parsed[0]
         path = parsed[1]
@@ -59,13 +59,13 @@ def main():
 
     # read and process all the arguments
     for name, file in test_list_files.items():
-        with open(file, "r") as f:
+        with open(file, 'r') as f:
             lines = f.readlines()
 
         test_lists[name] = []
         test_list = test_lists[name]
         for l in lines:
-            l = l.split("#", 1)[0]
+            l = l.split('#', 1)[0]
             l = l.strip()
             if len(l) == 0:
                 continue
@@ -73,31 +73,31 @@ def main():
             all_tests[l] = 1
 
     # generate the .cpp file
-    with open(cpp_file, "w") as f:
+    with open(cpp_file, 'w') as f:
         f.write(f'#include "{h_file_basename}"\n\n')
-        decls = [ f"extern struct test _test_{test};" for test in sorted(all_tests.keys()) ]
-        f.write("\n".join(decls))
-        f.write("\n\n")
+        decls = [ f'extern struct test _test_{test};' for test in sorted(all_tests.keys()) ]
+        f.write('\n'.join(decls))
+        f.write('\n\n')
         for name, tests in test_lists.items():
-            f.write(f"static constexpr struct test *{name}_test_list[] = {{")
-            test_list = [ f"    &_test_{test}" for test in tests ]
-            f.write(",\n".join(test_list))
-            f.write("\n};\n\n")
+            f.write(f'static constexpr struct test *{name}_test_list[] = {{')
+            test_list = [ f'    &_test_{test}' for test in tests ]
+            f.write(',\n'.join(test_list))
+            f.write('\n};\n\n')
         # write selector function
-        f.write("struct test * const *get_test_list(const char *platform, size_t *size) {\n")
+        f.write('struct test * const *get_test_list(const char *platform, size_t *size) {\n')
         if default_name is not None:
             f.write(f'    if (!platform) {{ *size = sizeof({default_name}_test_list)/sizeof(struct test *); return (struct test * const *)&{default_name}_test_list; }}\n')
         for name in test_lists.keys():
             f.write(f'    if  (!strcmp(platform, "{name}")) {{ *size = sizeof({name}_test_list)/sizeof(struct test *); return (struct test * const *)&{name}_test_list; }}\n')
-        f.write("    return nullptr;\n")
-        f.write("}\n")
+        f.write('    return nullptr;\n')
+        f.write('}\n')
 
     # generate .h file
-    with open(h_file, "w") as f:
-        f.write("#include \"sandstone.h\"\n")
-        f.write("struct test * const *get_test_list(const char *, size_t *);\n")
+    with open(h_file, 'w') as f:
+        f.write('#include "sandstone.h"\n')
+        f.write('struct test * const *get_test_list(const char *, size_t *);\n')
 
     exit(0)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
