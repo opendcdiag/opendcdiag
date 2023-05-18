a17b1e0727eb61a9ddd5ca8222791b33b0cef486
Athenas Jimenez <athenas.jimenez.gonzalez@intel.com>
2023-05-18 14:21:49 -0400

    FW: Add unittests utils
    Adding unittests utilities header so individual unittests can take
    advantage of them and to avoid definition conflicts in the future.
    
    Signed-off-by: Athenas Jimenez <athenas.jimenez.gonzalez@intel.com>

---
+++ framework/sandstone_unittests_utils.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2023 Intel Corporation.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "sandstone.h"
+
+#define UNITTESTS_NUM_CPUS 16
+
+/* Define dummy setup struct that can be used by unittests when needed */
+struct cpu_info *cpu_info = nullptr;
+
+/* Define dummy number of dummy cpus */
+int num_cpus() { return UNITTESTS_NUM_CPUS; }
+
