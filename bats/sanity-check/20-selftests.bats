opendcdiag/opendcdiag

commit fbc8a536c9d8eefe5fd3b4835fda93eca0b6b82b

FW: print the "--cpuset matched nothing" error in parse_cpuset_param()
We can easily count how many matches we had there.

Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

framework/sandstone.cpp
@@ -2007,6 +2007,7 @@ static LogicalProcessorSet parse_cpuset_param(char *arg)
     LogicalProcessorSet sys_cpuset = ambient_logical_processor_set();
     LogicalProcessorSet result = {};
     int max_cpu_count = sys_cpuset.count();
+    int total_matches = 0;
     if (cpu_info == nullptr)
         load_cpu_info(sys_cpuset);
 
@@ -2066,7 +2067,14 @@ static LogicalProcessorSet parse_cpuset_param(char *arg)
 
             if (match_count == 0)
                 fprintf(stderr, "%s: warning: CPU selection '%c%lu' matched nothing\n", program_invocation_name, c, n);
+            total_matches += match_count;
         }
+    }
+
+    if (total_matches == 0) {
+        fprintf(stderr, "%s: error: --cpuset matched nothing, this is probably not what you wanted.\n",
+                program_invocation_name);
+        exit(EX_USAGE);
     }
 
     return result;
@@ -3108,10 +3116,6 @@ int main(int argc, char **argv)
         case cpuset_option:
             sApp->enabled_cpus = parse_cpuset_param(optarg);
             sApp->thread_count = sApp->enabled_cpus.count();
-            if (sApp->thread_count == 0) {
-                fprintf(stderr, "%s: error: --cpuset matched nothing, this is probably not what you wanted.\n", argv[0]);
-                return EX_USAGE;
-            }
             break;
         case dump_cpu_info_option:
             dump_cpu_info(sApp->enabled_cpus);

commit 92dc5aaee87d9fd66e0007bb1ec68636a27d0315

FW: fix running with --cpuset <number>
The BATS tests only tested --cpuset <topology>, so this got broken
accidentally in fbc8a536c9d8eefe5fd3b4835fda93eca0b6b82b.

Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

framework/sandstone.cpp
@@ -2037,6 +2037,7 @@ static LogicalProcessorSet parse_cpuset_param(char *arg)
                 exit(EX_USAGE);
             }
             result.set(LogicalProcessor(n));
+            ++total_matches;
         } else {
             int i;
             int match_count = 0;

bats/sanity-check/20-selftests.bats
@@ -1118,3 +1118,31 @@ selftest_crash_context_common() {
     test_yaml_numeric "/triage-results@len" 'value == 1'
     test_yaml_numeric "/triage-results/0" 'value == 1'
 }
+
+function selftest_cpuset() {
+    local expected_logical=$1
+    local expected_package=$2
+    local expected_core=$3
+    local expected_thread=$4
+    shift 4
+
+    declare -A yamldump
+    sandstone_selftest -vvv -e selftest_pass "$@"
+    [[ "$status" -eq 0 ]]
+    test_yaml_numeric "/tests/0/threads/1/id/logical" "value == $expected_logical"
+    test_yaml_numeric "/tests/0/threads/1/id/package" "value == $expected_package"
+    test_yaml_numeric "/tests/0/threads/1/id/core" "value == $expected_core"
+    test_yaml_numeric "/tests/0/threads/1/id/thread" "value == $expected_thread"
+}
+
+@test "cpuset=number (first)" {
+    # Get the first logical processor
+    local -a cpuinfo=(`$SANDSTONE --dump-cpu-info | sed -n '/^[0-9]/{p;q;}'`)
+    selftest_cpuset ${cpuinfo[0]} ${cpuinfo[1]} ${cpuinfo[2]} ${cpuinfo[3]} --cpuset=${cpuinfo[0]}
+}
+
+@test "cpuset=number (last)" {
+    # Get the last logical processor
+    local -a cpuinfo=(`$SANDSTONE --dump-cpu-info | sed -n '$p'`)
+    selftest_cpuset ${cpuinfo[0]} ${cpuinfo[1]} ${cpuinfo[2]} ${cpuinfo[3]} --cpuset=${cpuinfo[0]}
+}