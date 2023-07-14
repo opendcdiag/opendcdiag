9539ef1205b51efb56381c8494e21c37e327b308
Thiago Macieira <thiago.macieira@intel.com>
2023-07-14 11:48:15 -0700

    FW/Topology: add support for pinning to a CPU range
    Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

--- framework/topology.h
+++ framework/topology.h
@@ -184,6 +184,7 @@ private:
 
 LogicalProcessorSet ambient_logical_processor_set();
 bool pin_to_logical_processor(LogicalProcessor, const char *thread_name = nullptr);
+bool pin_to_logical_processors(CpuRange, const char *thread_name);
 
 void apply_cpuset_param(char *param);
 void init_topology(const LogicalProcessorSet &enabled_cpus);
