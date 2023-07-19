262b6c47c26844c03101782c45f5a6d478f281db
Thiago Macieira <thiago.macieira@intel.com>
2023-07-19 13:48:00 -0700

    FW: pre-allocate the stacks' threads
    There are two reasons for this. First, it will allow us to randomise the
    stack addresses according to the fracture, replacing the code that was in
    `run_one_test()` and didn't do anything. Second, because it shows a
    performance improvement:
    
    |  # Threads   | Before (µs)  |  After (µs)  |
    |--------------|--------------|--------------|
    |            1 |        35950 |        33363 |
    |           24 |        53527 |        53386 |
    |           48 |        57227 |        55624 |
    |           96 |        62519 |        59104 |
    |          192 |        74021 |        67249 |
    
    Subtracting the first line from all others:
    
    |            |        Before (µs)      |        After (µs)       |
    |  # Threads |      Total | Per thread |      Total | Per thread |
    |------------|------------|------------|------------|------------|
    |         23 |      17576 |        764 |      20022 |        871 |
    |         47 |      21277 |        453 |      22261 |        474 |
    |         95 |      26568 |        280 |      25741 |        271 |
    |        191 |      38071 |        199 |      33886 |        177 |
    
    Even subtracting the overhead of starting a thread, the marginal cost of
    starting threads continues to decrease (I'm not sure why, I expected it
    to increase).
    
    I also benchmarked not having guard pages, but that seems to trigger a
    perverse optimisation inside the kernel, at least in the machine I
    tested: it decided to offer 2 MB huge pages, but unfortunately that's
    slower to allocate.
    
    Pre-faulting the top of the stack for each doesn't seem to help, because
    `pthread_create()` writes there anyway.
    
    Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

--- framework/sandstone_thread.cpp
+++ framework/sandstone_thread.cpp
@@ -8,14 +8,20 @@
 
 #include "gettid.h"
 
+#include <sys/mman.h>
+
 namespace {
 struct SandstoneTestThreadAttributes
 {
+    static constexpr size_t GuardSize = 8192;
     pthread_attr_t thread_attr;
+    unsigned char *stacks_block;
     SandstoneTestThreadAttributes()
+        : stacks_block(allocate_stack_block())
     {
         pthread_attr_init(&thread_attr);
         pthread_attr_setstacksize(&thread_attr, THREAD_STACK_SIZE);
+
     }
     ~SandstoneTestThreadAttributes()
     {
@@ -23,8 +29,47 @@ struct SandstoneTestThreadAttributes
     }
     SandstoneTestThreadAttributes(const SandstoneTestThreadAttributes &) = delete;
     SandstoneTestThreadAttributes &operator=(const SandstoneTestThreadAttributes &) = delete;
+
+    static unsigned char *allocate_stack_block();
+    void update_with_stack_for(int cpu);
 };
 } // unnamed namespace
+
+#ifndef _WIN32
+unsigned char *SandstoneTestThreadAttributes::allocate_stack_block()
+{
+    size_t size = num_cpus() * (THREAD_STACK_SIZE + GuardSize);
+    void *map = mmap(nullptr, size, PROT_NONE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+    if (map == MAP_FAILED)
+        return nullptr;
+
+    auto ptr = static_cast<unsigned char *>(map);
+    for (int i = 0; i < num_cpus(); ++i) {
+        ptr += GuardSize;
+        mprotect(ptr, THREAD_STACK_SIZE, PROT_READ | PROT_WRITE);
+        ptr += THREAD_STACK_SIZE;
+    }
+    return ptr;
+}
+
+void SandstoneTestThreadAttributes::update_with_stack_for(int cpu)
+{
+    if (!stacks_block)
+        return;
+    unsigned char *stacktop = stacks_block;
+    stacktop -= (THREAD_STACK_SIZE + GuardSize) * unsigned(cpu);
+    pthread_attr_setstack(&thread_attr, stacktop - THREAD_STACK_SIZE, THREAD_STACK_SIZE);
+}
+#else
+unsigned char *SandstoneTestThreadAttributes::allocate_stack_block()
+{
+    return nullptr;
+}
+void SandstoneTestThreadAttributes::update_with_stack_for(int)
+{
+}
+#endif
+
 
 [[maybe_unused]] static bool check_run_from_correct_thread()
 {
@@ -49,6 +94,7 @@ void SandstoneTestThread::start(RunnerFunction *f, int cpu)
 
     thread_num = cpu;
     target = f;
+    thread_attributes.update_with_stack_for(cpu);
     pthread_create(&thread, &thread_attributes.thread_attr, runner, this);
 }
 
