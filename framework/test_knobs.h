b3b6b5535199911d9b793651254e794ba4c878ec
Brad Kelly <brad.a.kelly@intel.com>
2023-08-18 10:05:59 -0700

    Added support for floating point knob values
    Signed-off-by: Brad Kelly <brad.a.kelly@intel.com>

--- framework/test_knobs.h
+++ framework/test_knobs.h
@@ -22,6 +22,7 @@ extern "C" {
 #  define get_testspecific_knob_value_uint(test, key, value_if_not_present)    (uint64_t)(value_if_not_present)
 #  define get_testspecific_knob_value_int(test, key, value_if_not_present)    (int64_t)(value_if_not_present)
 #  define get_testspecific_knob_value_string(test, key, value_if_not_present) (const char*)(value_if_not_present)
+#  define get_testspecific_knob_value_double(test, key, value_if_not_present) (double)(value_if_not_present)
 #  define set_knob_from_key_value_string(key_value_pair)     ((bool) true)
 #else
 
@@ -31,6 +32,8 @@ uint64_t get_testspecific_knob_value_uint(const struct test *test, const char *k
                                           uint64_t value_if_not_present);
 int64_t  get_testspecific_knob_value_int(const struct test *test, const char *key,
                                          int64_t value_if_not_present);
+double   get_testspecific_knob_value_double(const struct test *test, const char *key,
+                                            double value_if_not_present);
 const char *get_testspecific_knob_value_string(const struct test *test, const char *key,
                                                const char *value_if_not_present);
 
@@ -42,6 +45,8 @@ static inline uint64_t get_test_knob_value_uint(const char *key, uint64_t value_
 { return get_testspecific_knob_value_uint(NULL, key, value_if_not_present); }
 static inline int64_t  get_test_knob_value_int(const char *key, int64_t value_if_not_present)
 { return get_testspecific_knob_value_int(NULL, key, value_if_not_present); }
+static inline double  get_test_knob_value_double(const char *key, double value_if_not_present)
+{ return get_testspecific_knob_value_double(NULL, key, value_if_not_present); }
 static inline const char *get_test_knob_value_string(const char *key, const char *value_if_not_present)
 { return get_testspecific_knob_value_string(NULL, key, value_if_not_present); }
 
@@ -51,7 +56,7 @@ static inline const char *get_test_knob_value_string(const char *key, const char
 
 // implemented in logging.cpp (or unit test)
 enum class KnobOrigin { Options, Defaulted };
-using TestKnobValue = std::variant<std::string_view, uint64_t, int64_t>;
+using TestKnobValue = std::variant<std::string_view, uint64_t, int64_t, double>;
 void logging_mark_knob_used(std::string_view key, TestKnobValue value, KnobOrigin origin);
 #endif
 
