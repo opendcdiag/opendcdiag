ee7103c8334333a799cb5f08ebc5d64a05ae0b5c
Magdalena Cisowska <magdalena.cisowska@intel.com>
2025-07-24 10:09:33 +0200

    CPU-separation: actually define and use `SANDSTONE_DEVICE_CPU`
    Signed-off-by: Magdalena Cisowska <magdalena.cisowska@intel.com>

--- framework/device/device.h
+++ framework/device/device.h
@@ -6,8 +6,10 @@
 #ifndef INC_DEVICE_H
 #define INC_DEVICE_H
 
-// #ifdef SANDSTONE_DEVICE_CPU
+#include "sandstone_config.h"
+
+#if SANDSTONE_DEVICE_CPU
 #include <device/cpu/cpu_device.h>
-// #endif
+#endif
 
 #endif /* INC_DEVICE_H */
