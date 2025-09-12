opendcdiag/opendcdiag

commit 3994e19308d9b170ee5d62514d27234d368efccc

FW: re-disable stdout in debug builds
Updates 40f2b76248dd974948979a505fe66eac25702cd2, which allowed
`printf()` and family to print to stdout in debug builds, because
printf-debugging was useful with some libraries we had to use.

This retains that capability, but makes it an explicit opt-in, not
conditional in simply being a debug-mode build.

Drive-by remove the "no-triage" framework option.

Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

framework/logging.cpp
@@ -612,11 +612,17 @@ void logging_init_global(void)
         exit(EXIT_MEMORY);
     }
 
-#ifdef NDEBUG
-    // replace stdout with /dev/null
-    int devnull = open(_PATH_DEVNULL, O_RDWR | O_CLOEXEC);
-    dup2(devnull, STDOUT_FILENO);
-#endif
+    if (current_output_format() == SandstoneApplication::OutputFormat::no_output ||
+            !SandstoneConfig::AllowStdoutFromTests) {
+        // replace stdout with /dev/null
+        close(STDOUT_FILENO);
+        int devnull = open(_PATH_DEVNULL, O_RDWR | O_CLOEXEC);
+        assert(devnull >= 0);
+        if (devnull != STDOUT_FILENO) {
+            dup2(devnull, STDOUT_FILENO);
+            close(devnull);
+        }
+    }
 
     if (current_output_format() == SandstoneApplication::OutputFormat::no_output) {
         file_log_fd = STDOUT_FILENO;
@@ -661,10 +667,6 @@ void logging_init_global(void)
         }
 #endif
     }
-
-#ifdef NDEBUG
-    close(devnull);
-#endif
 
     /* open some place to store stderr in the child processes */
 #if !defined(__SANITIZE_ADDRESS__)

framework/meson.build
@@ -143,6 +143,10 @@ framework_config.set10(
     'SANDSTONE_RESTRICTED_CMDLINE',
     get_option('framework_options').contains('restricted-cmdline'),
 )
+framework_config.set10(
+    'SANDSTONE_ALLOW_STDOUT_FROM_TESTS',
+    get_option('framework_options').contains('allow-stdout-from-tests')
+)
 if get_option('framework_options').contains('no-child-debug')
     framework_config.set10('SANDSTONE_CHILD_DEBUG_CRASHES', false)
     framework_config.set10('SANDSTONE_CHILD_DEBUG_HANGS', false)

framework/sandstone_config.h.in
@@ -26,6 +26,7 @@
 #mesondefine SANDSTONE_SSL_BUILD
 #mesondefine SANDSTONE_SSL_LINKED
 
+#mesondefine SANDSTONE_ALLOW_STDOUT_FROM_TESTS
 #mesondefine SANDSTONE_DEFAULT_LOGGING
 #mesondefine SANDSTONE_NO_LOGGING
 #mesondefine SANDSTONE_RESTRICTED_CMDLINE
@@ -45,6 +46,7 @@ static constexpr bool Debug = DEBUG;
 static constexpr bool StaticLink = SANDSTONE_STATIC;
 
 // keep alphabetical order, please
+static constexpr bool AllowStdoutFromTests = SANDSTONE_ALLOW_STDOUT_FROM_TESTS;
 static constexpr bool ChildBacktrace = SANDSTONE_CHILD_BACKTRACE;
 static constexpr bool ChildDebug = SANDSTONE_CHILD_DEBUG;
 static constexpr bool ChildDebugCrashes = SANDSTONE_CHILD_DEBUG_CRASHES;

commit 1a26ce1964ca237a6c380eecc3e1b2548b537458

FW/Logging: fix unexpected closing of stdout in `-fexec` mode
Amends commit 3994e19308d9b170ee5d62514d27234d368efccc, which introduced
this problem by opening "/dev/null" with `O_CLOEXEC`. I don't think this
affected Windows because the three standard handles are special.

Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

framework/logging.cpp
@@ -616,7 +616,7 @@ void logging_init_global(void)
             !SandstoneConfig::AllowStdoutFromTests) {
         // replace stdout with /dev/null
         close(STDOUT_FILENO);
-        int devnull = open(_PATH_DEVNULL, O_RDWR | O_CLOEXEC);
+        int devnull = open(_PATH_DEVNULL, O_RDWR);
         assert(devnull >= 0);
         if (devnull != STDOUT_FILENO) {
             dup2(devnull, STDOUT_FILENO);