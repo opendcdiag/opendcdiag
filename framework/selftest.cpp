929ddfe2bd31a49ccc60a968b091599c0cf8dc40
Thiago Macieira <thiago.macieira@intel.com>
2025-09-11 17:31:46 -0700

    FW: add flag `test_init_in_parent` to execute init() in the parent
    This allows certain tests that always, deterministically and with little
    overhead skip or fail to be run in the parent process, avoiding the need
    to create a child process and waiting on it. This is going to be useful
    for the placeholder tests that cannot be implemented in a given OS or
    configuration, especially so for Windows where `CreateProcess` is rather
    expensive.
    
    Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

--- framework/selftest.cpp
+++ framework/selftest.cpp
@@ -68,6 +68,14 @@ struct SelftestException : std::exception
     }
 };
 
+static void check_is_main_process()
+{
+    if (!sApp->is_main_process()) {
+        fprintf(stderr, "Function appears to have run in the child process\n");
+        abort();
+    }
+}
+
 static int selftest_pass_run(struct test *test, int cpu)
 {
     printf("# This was printed from the test. YOU SHOULD NOT SEE THIS!\n");
@@ -1068,6 +1076,12 @@ static int selftest_inject_idle(struct test *test, int cpu)
     return EXIT_SUCCESS;
 }
 
+template <initfunc F> static int selftest_init_mainproc(struct test *test)
+{
+    check_is_main_process();
+    return F(test);
+}
+
 const static test_group group_positive = {
     .id = "positive",
     .description = "Self-tests that succeed (positive results)"
@@ -1254,6 +1268,16 @@ static struct test selftests_array[] = {
     .test_run = selftest_noreturn_run,
     .desired_duration = -1,
     .quality_level = TEST_QUALITY_PROD,
+},
+{
+    .id = "selftest_log_skip_init_mainproc",
+    .description = "Skips using log_skip() in the init function in the main process",
+    .groups = DECLARE_TEST_GROUPS(&group_positive),
+    .test_init = selftest_init_mainproc<selftest_log_skip_init>,
+    .test_run = selftest_noreturn_run,
+    .desired_duration = -1,
+    .quality_level = TEST_QUALITY_PROD,
+    .flags = test_init_in_parent,
 },
 {
     .id = "selftest_log_skip_init_socket0",
@@ -1588,6 +1612,16 @@ static struct test selftests_array[] = {
     .test_run = selftest_failinit_run,
     .desired_duration = -1,
     .quality_level = TEST_QUALITY_PROD,
+},
+{
+    .id = "selftest_logerror_init_mainproc",
+    .description = "Fails on error logged in init from the main process",
+    .groups = DECLARE_TEST_GROUPS(&group_negative),
+    .test_init = selftest_init_mainproc<selftest_logerror_init>,
+    .test_run = selftest_failinit_run,
+    .desired_duration = -1,
+    .quality_level = TEST_QUALITY_PROD,
+    .flags = test_init_in_parent,
 },
 {
     .id = "selftest_logerror_and_logskip",
