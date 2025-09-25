opendcdiag/opendcdiag

commit 2f9653845f837aa5940e24e4c151def225e79f46

FW/CPU: enable checking of XSAVE states
We only enabled checking of the AMX and AVX states, not the other ones
in the `xsave_requirements[]` array. This now enables all of them.

In addition to APX-F, this also enables checking of the CET state.

Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

framework/device/cpu/cpuid_internal.h
@@ -201,40 +201,34 @@ static cpu_features_t detect_cpu()
         assert(avx10ver < 5 && "Internal error: update code above!");
     }
 
-    if (osxsave) {
-        uint64_t xcr0 = 0;
-        uint64_t xcr0_wanted = 0;
-
-        if (features & XSaveReq_AvxState)
-            xcr0_wanted |= XSave_AvxState;
-        if (features & XSaveReq_Avx512State)
-            xcr0_wanted |= XSave_Avx512State;
-        if (features & XSaveReq_AmxState)
-            xcr0_wanted |= XSave_AmxState;
-
-        if (xcr0_wanted) {
-            uint32_t xcr0_low, xcr0_high;
-            asm("xgetbv" : "=a" (xcr0_low), "=d" (xcr0_high) : "c" (0));
-            xcr0 = xcr0_low;
-            if (xcr0_wanted != (uint32_t)xcr0_wanted)
-                xcr0 |= (uint64_t)xcr0_high << 32;      // don't discard %edx
-            xcr0 = adjusted_xcr0(xcr0, xcr0_wanted);
-        }
-
-        // Check what XSAVE features this OS supports and we're allowed to use.
-        // For AMX, we (currently) allow gracefully degrading. For AVX512 and
-        // earlier, we stop execution.
-        if ((xcr0 & XSave_AmxState) != XSave_AmxState) {
-            xcr0_wanted &= ~XSave_AmxState;
-            features &= ~XSaveReq_AmxState;
-        }
-
-        if (xcr0_wanted && (xcr0 & xcr0_wanted) != xcr0_wanted)
-            detect_cpu_not_supported("This kernel did not enable necessary AVX or AMX state-saving."
-                                     " Cannot run.\n");
-
+    uint64_t xcr0 = 0;
+    uint64_t xcr0_wanted = 0;
+    for (const XSaveRequirementMapping xsavereq : xsave_requirements) {
+        if (features & xsavereq.cpu_features)
+            xcr0_wanted |= xsavereq.xsave_state;
     }
 
+    if (xcr0_wanted && osxsave) {
+        uint32_t xcr0_low, xcr0_high;
+        asm("xgetbv" : "=a" (xcr0_low), "=d" (xcr0_high) : "c" (0));
+        xcr0 = xcr0_low;
+        if (xcr0_wanted != (uint32_t)xcr0_wanted)
+            xcr0 |= (uint64_t)xcr0_high << 32;      // don't discard %edx
+        xcr0 = adjusted_xcr0(xcr0, xcr0_wanted);
+    }
+
+    // Check what XSAVE features this OS supports and we're allowed to use.
+    // We do not support running on AVX or AVX512-capable processors without
+    // the corresponding OS support.
+    if ((xcr0_wanted & XSave_Avx512State) != (xcr0 & XSave_Avx512State))
+        detect_cpu_not_supported("This kernel did not enable necessary AVX state-saving."
+                                 " Cannot run.\n");
+
+    // For everything else, gracefully degrade by disabling the feature.
+    for (const XSaveRequirementMapping xsavereq : xsave_requirements) {
+        if ((xcr0 & xsavereq.xsave_state) != xsavereq.xsave_state)
+            features &= ~xsavereq.cpu_features;
+    }
     return features;
 }
 

commit fd83266f8548d17f122b0bae01706bd684ace7f9

FW/CPU: fix Linux dynamic XSAVE state requests
The `adjusted_xcr0()` function was designed when we thought new XSAVE
states would be allocated at the end of the last, thus allowing us to
make a single request for the highest numbered feature and thus get them
all. That turned out not to be true: APX-F reused the storage previously
allocated for MPX (a deprecated feature), so the Linux kernel requires
no request. More importantly, if you do request feature 19, it replies
with -EOPNOTSUPP (see [1]), which causes us to conclude the kernel
supports neither APX-F nor AMX.

So rewrite this to make a request for each of the bits that we require,
instead of trying to make a single request to get them all. Note that
the kernel only requires and accepts the request to XTILEDATA for AMX,
enabling both it and XTILECFG.

[1] https://codebrowser.dev/linux/linux/arch/x86/kernel/fpu/xstate.c.html#xstate_request_perm

Fixes: 2f9653845f ("FW/CPU: enable checking of XSAVE states")
Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

framework/device/cpu/cpuid_internal.h
@@ -67,39 +67,23 @@ static uint64_t adjusted_xcr0(uint64_t xcr0, uint64_t xcr0_wanted)
 #  endif // ARCH_GET_XCOMP_SUPP
 static uint64_t adjusted_xcr0(uint64_t xcr0, uint64_t xcr0_wanted)
 {
-    static const uint64_t KernelNonDynamicXSave = XSave_Xtilecfg - 1;
+    // Bits that the Linux kernel requires us to dynamically request
+    static const uint64_t KernelNonDynamicXSave = XSave_Xtiledata;
 
     // Linux doesn't hide XCR0 bits
     xcr0 &= xcr0_wanted;
 
     // Check if we need to make a dynamic XSAVE request
-    if ((xcr0_wanted & ~KernelNonDynamicXSave) == 0)
-        return xcr0;            // no, XCR0 is accurate
+    uint64_t xcr0_needed = (xcr0_wanted & KernelNonDynamicXSave);
+    while (xcr0_needed) {
+        unsigned long feature_nr = __builtin_ctzll(xcr0_needed);
+        uint64_t bit = UINT64_C(1) << feature_nr;
 
-    // dynamic XSAVE support required, ask for everything
-    uint64_t feature_nr = 63 - __builtin_clzll(xcr0_wanted);
-    if (syscall(SYS_arch_prctl, ARCH_REQ_XCOMP_PERM, feature_nr) == 0)
-        return xcr0_wanted;     // we got it
-
-    // Either ARCH_REQ_XCOMP_PERM isn't supported or the kernel doesn't support
-    // XSAVE'ing the feature we asked for (and we can't tell from the errno, since
-    // it returns EINVAL for both situations). Ask the kernel what it does support.
-    uint64_t xcr0_supported;
-    if (syscall(SYS_arch_prctl, ARCH_GET_XCOMP_SUPP, &xcr0_supported) == 0) {
-        // The call is supported (Linux >= 5.16). Ask for the highest bit that
-        // we want and the kernel supports.
-        xcr0_wanted &= xcr0_supported;
-        feature_nr = 63 - __builtin_clzll(xcr0_wanted);
-        if (syscall(SYS_arch_prctl, ARCH_REQ_XCOMP_PERM, feature_nr) == 0)
-            return xcr0_wanted;     // we got it
+        // ask the kernel to enable this feature; disable if it doesn't support
+        if (syscall(SYS_arch_prctl, ARCH_REQ_XCOMP_PERM, feature_nr) != 0)
+            xcr0 &= ~bit;
+        xcr0_needed &= ~bit;
     }
-
-    // Either Linux < 5.16 or the kernel failed to enable what it told us it
-    // supported (can happen if something else has installed a sigaltstack()
-    // that is too small).
-#  ifndef LINUX_COMPAT_PRE_5_16_AMX_SUPPORT
-    xcr0 &= KernelNonDynamicXSave;
-#  endif
     return xcr0;
 }
 #else