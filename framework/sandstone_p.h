7b7ccff37e7f9e832de93c79bb0b60c277264e0f
Thiago Macieira <thiago.macieira@intel.com>
2025-10-03 09:05:55 -0700

    Revert "FW: bring back `mce_counts_start` as `SandstoneApplication`s field"
    This reverts commit ceefdb3102c8a19457abf2982755b07af4d89d54.

--- framework/sandstone_p.h
+++ framework/sandstone_p.h
@@ -423,7 +423,6 @@ struct SandstoneApplication : public test_the_test_data<SandstoneConfig::Debug>
     static constexpr int DefaultTemperatureThreshold = -1;
     int thermal_throttle_temp = DefaultTemperatureThreshold;
     int threshold_time_remaining = 30000;
-    std::vector<uint32_t> mce_counts_start;
 
     HardwareInfo hwinfo;
 
