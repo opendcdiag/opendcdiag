5fb2d3ff25442e768e4d9ebcc00c1c18c48760bd
Thiago Macieira <thiago.macieira@intel.com>
2025-10-16 17:26:10 -0700

    FW/Logging: prepare to add `memcmp_or_fail()` with callbacks
    This commit prepares the internal functions for accepting a callback
    from test code, which is why the definitions are in sandstone.h not in
    the private headers.
    
    Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

--- framework/sandstone_p.h
+++ framework/sandstone_p.h
@@ -628,7 +628,9 @@ void logging_mark_thread_skipped(int thread_num) noexcept;
 void logging_cleanup_failure_callback() noexcept;
 void logging_run_callback();
 void logging_report_mismatched_data(enum DataType type, const uint8_t *actual, const uint8_t *expected,
-                                    size_t size, ptrdiff_t offset, const char *fmt, va_list);
+                                    size_t size, ptrdiff_t offset,
+                                    SandstoneMemcmpOrFail::FormatterCallback formatter,
+                                    const void *token1, void *token2);
 void logging_print_header(int argc, char **argv, ShortDuration test_duration, ShortDuration test_timeout);
 void logging_print_iteration_start();
 void logging_print_footer();
