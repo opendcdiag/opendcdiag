30962e476fb89be035c0e9d2c77fc2dfe35e8b4d
Magdalena Cisowska <magdalena.cisowska@intel.com>
2025-11-12 09:25:17 +0100

    unittests: define `thread_count()`
    Signed-off-by: Magdalena Cisowska <magdalena.cisowska@intel.com>

--- framework/sandstone_unittests_utils.cpp
+++ framework/sandstone_unittests_utils.cpp
@@ -5,13 +5,14 @@
 
 #include "sandstone.h"
 
-#define UNITTESTS_NUM_CPUS 16
+#define UNITTESTS_THREAD_COUNT 16
 
 /* Define dummy setup struct that can be used by unittests when needed */
 device_info_t *cpu_info = nullptr;
 
-/* Define dummy number of dummy cpus */
-int num_cpus() { return UNITTESTS_NUM_CPUS; }
+/* Define dummy number of dummy threads */
+int thread_count() { return UNITTESTS_THREAD_COUNT; }
+int num_cpus() { return thread_count(); }
 
 bool test_is_retry() noexcept { return false; }
 
