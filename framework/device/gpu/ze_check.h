5a671162efe7d39cbb5980ff3024c2ebc1967bdb
Magdalena Cisowska <magdalena.cisowska@intel.com>
2026-01-12 17:40:51 +0100

    FW/GPU: add helper functions for enumerating L0 resources
    L0 API is C-based, which introduces a lot of boilerplate code.
    Helper functions grouped in `ze_enumeration.*` allow to enumerate
    drivers, devices, and commonly used properties. File `ze_utils.*`
    provides conversion functions, converting codes into strings. `ze_check.h`
    provides a macro for calling L0 functions.
    
    Signed-off-by: Magdalena Cisowska <magdalena.cisowska@intel.com>

---
+++ framework/device/gpu/ze_check.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2026 Intel Corporation.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef INC_ZE_CHECK_H
+#define INC_ZE_CHECK_H
+
+#include "sandstone_p.h"
+#include "ze_utils.h"
+
+#include "level_zero/ze_api.h"
+
+/// Check return value of an L0 API call. Log the return status and return EXIT_FAILURE if check against "success" status fails.
+#define ZE_CHECK(...) \
+    do { \
+        auto result = (__VA_ARGS__); \
+        if (result != ZE_RESULT_SUCCESS) { \
+            if (thread_num >= 0) { \
+                log_debug("L0 API call failed with status %s", to_string(result)); \
+            } else { \
+                logging_printf(LOG_LEVEL_VERBOSE(1), "L0 API call failed with status %s\n", to_string(result)); \
+            } \
+            return EXIT_FAILURE; \
+        } \
+    } while (0)
+
+#endif // INC_ZE_CHECK_H
