34cc01d10bd4d5ded3d1a1f2200de03eb096adac
Thiago Macieira <thiago.macieira@intel.com>
2026-02-09 17:46:42 -0800

    FW: Add a short ID to each test
    This is added as a pre-calculated value (with a compile-time script) so
    we can declare a variable with the short ID in the name, aliased to the
    same test variable. That will cause the linker to fail in case there is
    a collision.
    
    The `mce_check` is special because it doesn't use the `DECLARE_TEST`
    macro (because we don't want to put it in the "tests" section to be
    auto-selected), so it needs to be handled especially by the script. That
    `#if 0` is temporary and will be removed by the next commit.
    
    Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

--- framework/unit-tests/tests_dummy.cpp
+++ framework/unit-tests/tests_dummy.cpp
@@ -8,6 +8,6 @@
 // This defines a dummy test just so the __start_tests and __stop_tests
 // symbols get defined by the linker.
 
-DECLARE_TEST_INNER2(dummy, nullptr)
+DECLARE_TEST_INNER2(dummy, 1, nullptr)
 END_DECLARE_TEST
 
