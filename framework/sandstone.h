opendcdiag/opendcdiag

commit aba09c034226c0931f82486cb6833b63bd9746d3

FW: add `no-test-names` framework option
Which is implied if no logging is wanted, to save on size.

This allows us to hide test names and use only the short ID, which the
`DECLARE_TEST` macro will stringify (setting `test->id` to a null
pointer is left as an exercise for later and possibly only makes sense
in no-logging mode).

Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

framework/sandstone.h
@@ -164,8 +164,12 @@ typedef enum TestQuality {
 #define DECLARE_TEST_INNER(test_id, test_short_id, test_description)    \
     DECLARE_TEST_INNER2(test_id, test_short_id, test_description)
 
-#ifndef DECLARE_TEST
-#  define DECLARE_TEST(test_id, test_description)       DECLARE_TEST_INNER(test_id, TEST_ID_ ## test_id, test_description)
+#if SANDSTONE_NO_TEST_NAMES
+#  define DECLARE_TEST(test_id, test_description)                       \
+    DECLARE_TEST_INNER(TEST_ID_ ## test_id, TEST_ID_ ## test_id, NULL)
+#else
+#  define DECLARE_TEST(test_id, test_description)                       \
+    DECLARE_TEST_INNER(test_id, TEST_ID_ ## test_id, test_description)
 #endif
 
 #define DECLARE_TEST_GROUPS(...)                                      \

framework/meson.build
@@ -143,7 +143,11 @@ framework_config.set(
     'SANDSTONE_DEFAULT_LOGGING',
     'TestConfig::OutputFormat::' + get_option('logging_format'),
 )
-
+framework_config.set10(
+    'SANDSTONE_NO_TEST_NAMES',
+    get_option('framework_options').contains('no-test-names') or
+    get_option('logging_format') == 'no_output'
+)
 framework_config.set10(
     'SANDSTONE_RESTRICTED_CMDLINE',
     get_option('framework_options').contains('restricted-cmdline'),

framework/sandstone.cpp
@@ -555,8 +555,10 @@ get_next_test(SandstoneTestSet::EnabledTestList::iterator next_test)
         }
     }
 
+#if !SANDSTONE_NO_TEST_NAMES
     assert(next_test->test->id);
     assert(strlen(next_test->test->id));
+#endif
     assert(SandstoneConfig::NoLogging || next_test->test->description);
     return next_test;
 }

framework/sandstone_config.h.in
@@ -29,6 +29,7 @@
 #mesondefine SANDSTONE_ALLOW_STDOUT_FROM_TESTS
 #mesondefine SANDSTONE_DEFAULT_LOGGING
 #mesondefine SANDSTONE_NO_LOGGING
+#mesondefine SANDSTONE_NO_TEST_NAMES
 #mesondefine SANDSTONE_RESTRICTED_CMDLINE
 #mesondefine SANDSTONE_CHILD_BACKTRACE
 #mesondefine SANDSTONE_CHILD_DEBUG_CRASHES

tests/common/mce_check/mce_check.cpp
@@ -129,13 +129,13 @@ static_assert(!InterruptMonitor::InterruptMonitorWorks);
 #define CONCAT2(x, y)    x ## y
 struct test mce_test = {
         .shortid = CONCAT(0x, TEST_ID_mce_check),
-#if 0
+#if SANDSTONE_NO_TEST_NAMES
         .id = SANDSTONE_STRINGIFY(TEST_ID_mce_check),
         .description = nullptr,
 #else
         .id = "mce_check",
         .description = "Machine Check Exceptions/Events count",
-#endif // TEST_ID_mce_check
+#endif // SANDSTONE_NO_TEST_NAMES
 
 #if defined(__linux__) && defined(__x86_64__)
         .test_preinit = mce_check_preinit,

commit ed4777307c13e5be591ecd23e5d1a2dd0111ee80

FW: fix build with no-test-names and a built-in test list
The sandstone_test_lists.cpp file refers to the test variables by
name, which we broke in commit aba09c034226c0931f82486cb6833b63bd9746d3.

```
ld: opendcdiag/framework/libframework.a.p/meson-generated_.._sandstone_test_lists.cpp.o:(.data.rel.ro+0x2e70): undefined reference to `_test_eigen_svd_double'
ld: opendcdiag/framework/libframework.a.p/meson-generated_.._sandstone_test_lists.cpp.o:(.data.rel.ro+0x2c58): undefined reference to `_test_eigen_gemm_cdouble_dynamic_square'
ld: opendcdiag/framework/libframework.a.p/meson-generated_.._sandstone_test_lists.cpp.o:(.data.rel.ro+0x2c60): undefined reference to `_test_eigen_svd_cdouble'
ld: opendcdiag/framework/libframework.a.p/meson-generated_.._sandstone_test_lists.cpp.o:(.data.rel.ro+0x2c68): undefined reference to `_test_eigen_svd'
ld: opendcdiag/framework/libframework.a.p/meson-generated_.._sandstone_test_lists.cpp.o:(.data.rel.ro+0x2c70): undefined reference to `_test_zlib'
```

The variable names can be left behind, because they disappear when the
executable is stripped of debugging information.

This isn't tested in the GitHub Actions CI, so it wasn't caught.

Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

framework/sandstone.h
@@ -153,24 +153,23 @@ typedef enum TestQuality {
 #define EXIT_SKIP               -255
 
 /// force the alignment so the compiler won't try to (un)helpfully overalign it.
-#define DECLARE_TEST_INNER2(test_id, test_short_id, test_description)   \
+#define DECLARE_TEST_INNER2(test_id, test_id_str, test_short_id, test_description)  \
     __attribute__((alias("_test_" SANDSTONE_STRINGIFY(test_id)))) extern struct test _testid_ ## test_short_id; \
     __attribute__((aligned(alignof(struct test)), used, section(SANDSTONE_SECTION_PREFIX "tests"))) \
     struct test _test_ ## test_id = {                                   \
         .compiler_minimum_device = device_compiler_features,            \
         .shortid = 0x ## test_short_id,                                 \
-        .id = SANDSTONE_STRINGIFY(test_id),                             \
+        .id = SANDSTONE_STRINGIFY(test_id_str),                         \
         .description = test_description,
-#define DECLARE_TEST_INNER(test_id, test_short_id, test_description)    \
-    DECLARE_TEST_INNER2(test_id, test_short_id, test_description)
-
 #if SANDSTONE_NO_TEST_NAMES
-#  define DECLARE_TEST(test_id, test_description)                       \
-    DECLARE_TEST_INNER(TEST_ID_ ## test_id, TEST_ID_ ## test_id, NULL)
+#  define DECLARE_TEST_INNER(test_id, test_short_id, test_description)  \
+    DECLARE_TEST_INNER2(test_id, test_short_id, test_short_id, NULL)
 #else
-#  define DECLARE_TEST(test_id, test_description)                       \
-    DECLARE_TEST_INNER(test_id, TEST_ID_ ## test_id, test_description)
+#  define DECLARE_TEST_INNER(test_id, test_short_id, test_description)  \
+    DECLARE_TEST_INNER2(test_id, test_id, test_short_id, test_description)
 #endif
+#define DECLARE_TEST(test_id, test_description)                         \
+    DECLARE_TEST_INNER(test_id, TEST_ID_ ## test_id, test_description)
 
 #define DECLARE_TEST_GROUPS(...)                                      \
     __extension__ (const struct test_group* const[]){ __VA_ARGS__, NULL }

framework/unit-tests/tests_dummy.cpp
@@ -8,6 +8,6 @@
 // This defines a dummy test just so the __start_tests and __stop_tests
 // symbols get defined by the linker.
 
-DECLARE_TEST_INNER2(dummy, 1, nullptr)
+DECLARE_TEST_INNER(dummy, 1, nullptr)
 END_DECLARE_TEST
 