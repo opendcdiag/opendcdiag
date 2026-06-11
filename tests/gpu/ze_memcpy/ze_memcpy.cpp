9538467c32aaabbf72161eccf6ca8e191c9be173
Magdalena Cisowska <magdalena.cisowska@intel.com>
2026-06-11 09:20:35 +0200

    Tests/GPU: check L0 handles after enumeration
    Signed-off-by: Magdalena Cisowska <magdalena.cisowska@intel.com>

--- tests/gpu/ze_memcpy/ze_memcpy.cpp
+++ tests/gpu/ze_memcpy/ze_memcpy.cpp
@@ -38,11 +38,18 @@ int ze_memcpy_init(struct test* test)
     auto data = new ze_memcpy_data;
 
     // TODO: to be put in the common part for each L0 test
-    for_each_ze_device_within_topo([&](ze_device_handle_t device_handle, ze_driver_handle_t driver, const MultiSliceGpu& indices) {
+    int ret = for_each_ze_device_within_topo([&](ze_device_handle_t device_handle, ze_driver_handle_t driver, const MultiSliceGpu&) {
+        if (!device_handle || !driver)
+            return EXIT_FAILURE;
         data->ze_handles.emplace_back(device_handle);
         data->ze_driver = driver;
         return EXIT_SUCCESS;
     });
+    if (ret != EXIT_SUCCESS) {
+        log_error("Devices enumeration failure");
+        delete data;
+        return EXIT_FAILURE;
+    }
 
     data->context = ze_create_context(data->ze_driver);
     if (!data->context) {
