eeef3cb150e4b663c0bd62f2f99ebca4f321cbaa
Magdalena Cisowska <magdalena.cisowska@intel.com>
2026-08-25 13:18:11 +0200

    FW/IDXD: implement `thread_id_header_for_device`
    Example:
    ```
      - thread: 0
        id: { device: iax1, wq: 4, logical_cpu: 12, pci_address: 0000:6c:01.0 }
    ```
    
    Signed-off-by: Magdalena Cisowska <magdalena.cisowska@intel.com>

--- framework/device/idxd/logging_idxd.cpp
+++ framework/device/idxd/logging_idxd.cpp
@@ -4,11 +4,54 @@
  */
 
 #include "logging.h"
+#include "idxd_device.h"
+
+#include <algorithm>
+#include <format>
+#include <string>
 
 #if !SANDSTONE_NO_LOGGING
+static std::string to_string(accfg_device_type device_type)
+{
+    switch (device_type) {
+    case ACCFG_DEVICE_DSA:
+        return "dsa";
+    case ACCFG_DEVICE_IAX:
+        return "iax";
+    default:
+        return {};
+    }
+}
+
+static auto calc_spacing()
+{
+    // Note: this assumes the topology won't change after the first time this
+    // function is called. And it shouldn't, in the sense of devices' ids at least.
+    static const auto spacing = []() {
+        int res = 0;
+        auto max_cpu = std::max_element(
+            device_info, device_info + thread_count(),
+            [](const auto& i1, const auto& i2) { return i1.cpu_number < i2.cpu_number; }
+        )->cpu_number;
+        do { max_cpu /= 10; res++; } while (max_cpu != 0);
+        return res;
+    }();
+    return spacing;
+}
+
 std::string AbstractLogger::thread_id_header_for_device(int thread, LogLevelVerbosity verbosity)
 {
+    const wq_info_t* info = device_info + thread;
     std::string line;
+
+    line = std::format("{{ device: {}{}, wq: {}, ", to_string(info->dev_type), info->device_id, info->wq_id);
+    line += std::format("logical_cpu: {:{}}, ", info->cpu_number, calc_spacing());
+    line += std::format("pci_address: {:04x}:{:02x}:{:02x}.{:01x}",
+        info->bdf.domain, info->bdf.bus, info->bdf.device, info->bdf.function
+    );
+    line += " }";
+    // TODO: should we print mutable data as well?
+
     return line;
 }
 
